323bdb7e1ed07e008341eafa4b389e3b937f771c
Anandaji <53898330+Anandaji@users.noreply.github.com>
2019-08-16 07:20:26 +0530

    Update vow.c

--- vow.c
+++ vow.c
@@ -1,1 +1,20 @@
+#include <stdio.h>
+
+int main()
+{
+    char c;
+    printf("Enter the character ");
+    scanf("%c",&c);
+    if((c>='a' && c<='z') || (c>= 'A' && c<= 'Z'))
+    {
+        
+    if(c=='a'|| c=='e'|| c=='i'||c=='o'||c=='u'||c=='A'||c=='E'||c=='I'||c=='O'||c=='U' )
+        printf("Vowel");
+    else
+        printf("Consonant");
+    
+    }
+    else
+    printf("Invalid");
+}
 
