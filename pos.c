e2a27f91699aeab43e88d695791616253182b6fc
Anandaji <53898330+Anandaji@users.noreply.github.com>
2019-08-16 06:46:12 +0530

    Update pos.c

--- pos.c
+++ pos.c
@@ -1,1 +1,16 @@
+#include <stdio.h>
 
+int main()
+{
+    int a;
+    printf("Enter the number: ");
+    scanf("%d",&a);
+     if(a>0)
+        printf("Positive");
+    else if(a<0)
+        printf("Negative");
+    else
+        printf("Zero");
+ 
+        
+}
