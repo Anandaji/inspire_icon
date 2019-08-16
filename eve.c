c4db9f4b91588f0771a0144ef049776934d4fd11
Anandaji <53898330+Anandaji@users.noreply.github.com>
2019-08-16 06:56:28 +0530

    Update eve.c

--- eve.c
+++ eve.c
@@ -1,1 +1,16 @@
+#include <stdio.h>
 
+int main()
+{
+    int a;
+    printf("Enter the number: ");
+    scanf("%d",&a);
+     if(a<=0)
+        printf("invalid");
+    else if(a%2==0)
+        printf("Even");
+    else
+        printf("Odd");
+ 
+        
+}
